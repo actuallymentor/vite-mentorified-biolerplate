b752640384f6598d30af2d3ea5205258e5478470
Mentor <mentor@palokaj.co>
2022-07-25 17:26:34 +0200

    ✨ add functions boiler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Theme from './components/atoms/Theme'
 import Homepage from './components/organisms/Homepage'
+import { listen_to_document } from './modules/firebase'
 
 // ///////////////////////////////
 // Render component
